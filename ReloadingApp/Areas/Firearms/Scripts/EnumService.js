a761179467176e515ec108130a1550cf0be67a3c
farleyrw <farleyrw@gmail.com>
2014-01-01 20:46:46 -0500

    clean up

--- ReloadingApp/Areas/Firearms/Scripts/EnumService.js
+++ ReloadingApp/Areas/Firearms/Scripts/EnumService.js
@@ -2,15 +2,9 @@
 FirearmManager.service("FirearmEnumService",
 	["$resource",
         function (ajax) {
-        	var api = ajax('firearms/enums', { }, {
-        		Get: {
-        			method: 'GET'
-        		}
-        	});
+        	var api = ajax('firearms/enums', {}, { Get: { method: 'GET' } });
 
-		    return {
-			    Get: api.Get
-		    };
+		    return { Get: api.Get };
         }
 	]
 );
