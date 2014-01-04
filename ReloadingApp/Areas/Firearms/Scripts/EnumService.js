dc5c69d60c66e68cdb8af81cd9998a623c1e564d
farleyrw <farleyrw@gmail.com>
2014-01-04 13:45:11 -0500

    cache enum service requests

--- ReloadingApp/Areas/Firearms/Scripts/EnumService.js
+++ ReloadingApp/Areas/Firearms/Scripts/EnumService.js
@@ -2,7 +2,7 @@
 FirearmManager.service("FirearmEnumService",
 	["$resource",
         function (ajax) {
-        	var api = ajax('firearms/enums', {}, { Get: { method: 'GET' } });
+        	var api = ajax('firearms/enums', {}, { Get: { method: 'GET', cache: true } });
 
 		    return { Get: api.Get };
         }
