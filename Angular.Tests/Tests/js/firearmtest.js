10405b829bdf4687449deb37a3d2f5fb5aa5e43a
farleyrw <farleyrw@gmail.com>
2014-01-05 14:41:11 -0500

    add angular test project

---
+++ Angular.Tests/Tests/js/firearmtest.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+(function () {
+	describe('Controller init', function () {
+		var scope, controller, listServiceMock, firearms;
+
+		beforeEach(function () {
+			module('FirearmManager');
+
+			inject(function ($rootScope, $controller, ListService, EnumService) {
+				scope = $rootScope.$new();
+				firearms = [{ id: 1 }];
+				listServiceMock = sinon.stub(ListService);
+				listServiceMock.List.returns(firearms);
+				controller = $controller('FirearmListController', { $scope: scope });
+			});
+		});
+
+		it('should set the firearm list', function () {
+			expect(scope.firearms).toBe(firearms)
+		});
+	})
+})();
