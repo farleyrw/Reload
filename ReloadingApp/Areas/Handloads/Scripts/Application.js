226647500b624fff663fbda1bddf263f76c7b1ad
farleyrw <farleyrw@gmail.com>
2014-01-05 22:38:12 -0500

    create handload app

---
+++ ReloadingApp/Areas/Handloads/Scripts/Application.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+var HandloadManager = angular.module("HandloadManager", ['ngRoute', 'ngResource']);
+
+HandloadManager.config(['$routeProvider', function (routing) {
+	routing
+        .when('/list', { templateUrl: '/areas/handloads/templates/list.html', controller: 'HandloadListController' })
+        .when('/new', { templateUrl: '/areas/handloads/templates/edit.html', controller: 'HandloadEditController' })
+        //.when('/edit/:Id', { templateUrl: '/areas/firearms/templates/edit.html', controller: 'FirearmEditController' })
+        .otherwise({ redirectTo: '/list' });
+}]);
+
+// Filter to get the string value of the enum.
+FirearmManager.filter("EnumToString", function () {
+	return function (index, array) {
+		return array ? array[index].Name : index;
+	};
+});
