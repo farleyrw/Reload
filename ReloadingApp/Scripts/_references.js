e95ab37fffa93949f816f9ad1316e6514ea3d895
farleyrw <farleyrw@gmail.com>
2014-01-12 17:14:55 -0800

    add jquery intellisense reference

--- ReloadingApp/Scripts/_references.js
+++ ReloadingApp/Scripts/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="jquery/jquery-2.0.3.js" />
 /// <reference path="angular/angular.js" />
 /// <reference path="angular/angular-route.js" />
 /// <reference path="angular/angular-resource.js" />
