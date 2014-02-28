5add10bfbed4e587092cb07aaa7a4944e767b556
farley <farleyrw@gmail.com>
2014-02-27 23:11:44 -0500

    add custom json converter for parsing cartridge description attributes for incoming data

---
+++ ReloadingApp/Helpers/Json/CartridgeEnumDescriptionConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using Reload.Common.Enums;
+using Reload.Common.Helpers;
+
+namespace ReloadingApp.Helpers.Json
+{
+	public class CartridgeEnumDescriptionConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(Cartridge);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return EnumHelper.ParseDescription<Cartridge>(existingValue.ToString());
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			string enumValue = EnumHelper.Description<Cartridge>((Cartridge)value);
+
+			serializer.Serialize(writer, enumValue);
+		}
+	}
+}
