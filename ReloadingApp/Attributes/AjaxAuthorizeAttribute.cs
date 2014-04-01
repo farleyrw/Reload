dfb30e8f0ec8a219bf4b927c9095664b0c44c628
farley <farleyrw@gmail.com>
2014-04-01 18:36:12 -0400

    stub ajax authorize attribute

--- ReloadingApp/Attributes/AjaxAuthorizeAttribute.cs
+++ ReloadingApp/Attributes/AjaxAuthorizeAttribute.cs
@@ -4,5 +4,11 @@ namespace ReloadingApp.Attributes
 {
 	public class AjaxAuthorizeAttribute : AuthorizeAttribute
 	{
+		protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+		{
+			// TODO: Check if request is ajax and return 403 with specific json response.
+			// http://stackoverflow.com/questions/2580596/how-do-you-handle-ajax-requests-when-user-is-not-authenticated
+			base.HandleUnauthorizedRequest(filterContext);
+		}
 	}
 }
