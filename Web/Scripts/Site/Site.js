d2f9cee3fe6f166234c0f11d40a6e4be9fc9aaf2
farleyrw <farleyrw@gmail.com>
2014-04-08 21:54:18 -0400

    add client side error handling for unauthorized ajax calls

--- Web/Scripts/Site/Site.js
+++ Web/Scripts/Site/Site.js
@@ -1,1 +1,14 @@
 ﻿'use strict';
+
+$.ajaxSetup({
+	cache: false
+});
+
+$(document).ajaxError(
+	function (e, request) {
+		if (request.status == 401) {
+			alert("Your session has expired. Please login again to continue.");
+			//window.location = "/account/logon";
+		}
+	}
+);
