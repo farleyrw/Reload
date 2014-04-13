cb101bfcccb7280dbf9fc8f112708fc2b5b21473
farley <farleyrw@gmail.com>
2014-04-12 21:28:35 -0400

    add angular ajax unauthenticated request handling

--- Web/Areas/Firearms/Scripts/Application.js
+++ Web/Areas/Firearms/Scripts/Application.js
@@ -1,6 +1,7 @@
 ﻿'use strict';
 
-var FirearmManager = angular.module("FirearmManager", ['ngRoute', 'ngResource', 'ui.bootstrap'])
+var FirearmManager = angular.module("FirearmManager", ['ng', 'ngRoute', 'ngResource', 'ui.bootstrap'])
+	.constant('loginUrl', '/account/logon')
 	.constant('templateUrl', 'areas/firearms/templates/')
 	.config(['$routeProvider', 'templateUrl', function (routing, templateUrl) {
 		routing
@@ -8,8 +9,23 @@ var FirearmManager = angular.module("FirearmManager", ['ngRoute', 'ngResource', 
 			.when('/new', { templateUrl: templateUrl + 'edit.html', controller: 'FirearmEditController' })
 			.when('/edit/:Id', { templateUrl: templateUrl + 'edit.html', controller: 'FirearmEditController' })
 			.otherwise({ redirectTo: '/list' });
-	}
-]);
+	}])
+	.config(['$httpProvider', 'loginUrl', function (provider, loginUrl) {
+		// Add custom request interceptor.
+		provider.interceptors.push(function () {
+			return {
+				responseError: function (response) {
+					console.log('unathenticated', response);
+					// Redirect to logon page on unauthenticated request.
+					if (response.status == 401) {
+						alert("Your session has expired. Please login again to continue.");
+						window.location = loginUrl;
+					}
+					return response; // || promise.reject(response);
+				}
+			};
+		});
+	}]);
 
 // Filter to get the string value of the enum.
 FirearmManager.filter("EnumToString", function () {
