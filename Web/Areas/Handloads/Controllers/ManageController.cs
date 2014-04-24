8034c6d68e5e305eb5e25377eb5b362856a0412b
farley <farleyrw@gmail.com>
2014-04-23 20:55:35 -0400

    update nuget packages, add handload controller

---
+++ Web/Areas/Handloads/Controllers/ManageController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Reload.Service.Interfaces;
+using Reload.Web.Controllers;
+
+namespace Reload.Web.Areas.Handloads.Controllers
+{
+	/// <summary>The handload manage controller.</summary>
+	public class ManageController : BaseController
+	{
+		/// <summary>The handload service.</summary>
+		private readonly IHandloadService Service;
+
+		/// <summary>Initializes a new instance of the <see cref="ManageController"/> class.</summary>
+		/// <param name="service">The service.</param>
+		public ManageController(IHandloadService service)
+		{
+			this.Service = service;
+		}
+
+		/// <summary>The index view result.</summary>
+		public ViewResult Index()
+		{
+			return View();
+		}
+	}
+}
