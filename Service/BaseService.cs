aab0bce597df2ecfb53d0d4c5e86f2a848af5efc
farley <farleyrw@gmail.com>
2014-07-12 22:45:58 -0400

    update nuget packages, comment out broken mvc route tester

--- Service/BaseService.cs
+++ Service/BaseService.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using Reload.Common.Authentication;
+
 namespace Reload.Service
 {
-	public abstract class BaseService {}
+	public abstract class BaseService : IHasIdentityData
+	{
+		/// <summary>Gets or sets the identity.</summary>
+		/// <value>The identity.</value>
+		public IUserIdentityData Identity { get; set; }
+	}
 }
