59a6fc4e62be92abf0d66d0ca5f73ca2520ffedf
farley <farleyrw@gmail.com>
2014-09-04 22:17:22 -0400

    I finally got some angular unit tests working

--- UnitTests/Angular.Tests/Tests/Firearms/FirearmManagerFixture.js
+++ UnitTests/Angular.Tests/Tests/Firearms/FirearmManagerFixture.js
@@ -1,4 +1,6 @@
 ﻿/// <reference path="../../../../web/scripts/angular/angular.js" />
+/// <reference path="../../../../web/scripts/angular/angular-route.js" />
+/// <reference path="../../../../web/scripts/angular/angular-resource.js" />
 /// <reference path="../../../../web/scripts/angular/angular-mocks.js" />
 /// <reference path="../../../../web/scripts/angular-ui/ui-bootstrap.js" />
 /// <reference path="../../../../web/scripts/jquery/jquery-2.1.1.js" />
@@ -12,21 +14,36 @@
 /// <reference path="../../../../web/areas/firearms/scripts/firearmmanager.js" />
 
 describe('Firearm Manager tests', function () {
-	beforeEach(function () {
-		module('FirearmManager');
-	});
+	beforeEach(module('FirearmManager'));
 
-	describe('ListController', function () {
-		it('should set the firearm list', function() {//inject(function ($controller, $location, FirearmService, EnumService) {
-			//var scope = {};
-			//var firearmListController = $controller("FirearmListController", {
-			//	scope: scope,
-			//	location: $location,
-			//	FirearmService: FirearmService,
-			//	EnumService: EnumService
-			//});
-			//expect(firearmListController).not.toBeNull();
-			//expect(scope.Firearms).toEqual([]);
-		});//);
+	describe('FirearmListController', function () {
+		var scope, controller, $httpBackend;
+
+		beforeEach(inject(function (_$httpBackend_, $rootScope, $controller, $location) {
+			$httpBackend = _$httpBackend_;
+			$httpBackend.expectGET('firearms/manage/list').respond([{ abc: '123' }]);
+			$httpBackend.expectGET('/reload/firearms/enums').respond({ abc: '123' });
+
+			scope = $rootScope.$new();
+			controller = $controller('FirearmListController', { $scope: scope, $location: $location });
+		}));
+
+		it('should create scope methods', function () {
+			expect(scope.Add).toBeDefined();
+			expect(scope.Edit).toBeDefined();
+			expect(scope.Delete).toBeDefined();
+		});
+
+		it('should create "Firearms" model with data fetched from xhr', function () {
+			$httpBackend.flush();
+
+			expect(scope.Firearms[0].abc).toBe('123');
+		});
+
+		it('should create "Enums" model with data fetched from xhr', function () {
+			$httpBackend.flush();
+
+			expect(scope.Enums.abc).toBe('123');
+		});
 	});
 });
