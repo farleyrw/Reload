4065841dc002888b6619334bd68f831524e4377b
farleyrw <farleyrw@gmail.com>
2014-10-21 22:05:29 -0400

    fix up authorization unit tests, they work now!

--- UnitTests/Angular.Tests/Tests/Reload/AuthorizationFixture.js
+++ UnitTests/Angular.Tests/Tests/Reload/AuthorizationFixture.js
@@ -10,44 +10,57 @@
 
 describe("Authorization module tests", function () {
 	var httpProvider;
+	var window = {
+		alert: jasmine.createSpy(),
+		location: {
+			reload: jasmine.createSpy()
+		}
+	};
 	var exampleUrl = '/reload';
 
-	beforeEach(module('Authorization', function ($httpProvider) {
+	beforeEach(module('Authorization', function ($httpProvider, $provide) {
 		httpProvider = $httpProvider;
+		// Provide window mock, angular doesn't allow you to mock this easily.
+		$provide.value('$window', window);
 	}));
 
-	beforeEach(inject(function ($httpBackend, AuthorizationService, $window) {
+	beforeEach(inject(function ($httpBackend, AuthorizationService, $http) {
 		this.httpBackend = $httpBackend;
 		this.AuthorizationService = AuthorizationService;
-
-		this.window = $window;
-		this.window.alert = jasmine.createSpy();
-		this.window.location = {
-			reload: jasmine.createSpy()
-		};
+		this.http = $http;
 	}));
 
 	describe('Authorization service tests', function () {
+		it('should have AuthorizationService as an interceptor', function () {
+			expect(httpProvider.interceptors).toContain('AuthorizationService');
+		});
+
+		it('should have xml http header set', function () {
+			expect(httpProvider.defaults.headers.common["X-Requested-With"]).toEqual('XMLHttpRequest');
+		});
+
 		it('should have AuthorizationService be defined', function () {
 			expect(this.AuthorizationService).toBeDefined();
 		});
 
-		it('should have AuthorizationService as an interceptor', function () {
-			expect(httpProvider.interceptors).toContain('AuthorizationService');
-		});
-		
 		it('should pass through a 200 response', function () {
-			this.httpBackend.whenGET(exampleUrl, null, function () {
-				expect(this.window.alert).not.toHaveBeenCalled();
-				expect(this.window.location.reload).not.toHaveBeenCalled();
-			}).respond(200, null);
+			this.httpBackend.expectGET(exampleUrl).respond(200);
+			this.http.get(exampleUrl);
+
+			this.httpBackend.flush();
+
+			expect(window.alert).not.toHaveBeenCalled();
+			expect(window.location.reload).not.toHaveBeenCalled();
 		});
 
-		it('should redirect on 401 response', function () {
-			this.httpBackend.whenGET(exampleUrl, null, function (headers) {
-				expect(this.window.alert).toHaveBeenCalled();
-				expect(this.window.location.reload).toHaveBeenCalled();
-			}).respond(401, null);
+		it('should alert and reload on 401 response', function () {
+			this.httpBackend.expectGET(exampleUrl).respond(401);
+			this.http.get(exampleUrl);
+
+			this.httpBackend.flush();
+
+			expect(window.alert).toHaveBeenCalled();
+			expect(window.location.reload).toHaveBeenCalled();
 		});
 	});
 });
