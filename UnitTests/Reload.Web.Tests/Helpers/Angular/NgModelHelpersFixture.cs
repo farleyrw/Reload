7a47bdb018e0dc72bc38058a614a5b825c1a7e88
farley <farleyrw@gmail.com>
2015-02-23 23:48:55 -0500

    html helper unit testing is beyond stupid

---
+++ UnitTests/Reload.Web.Tests/Helpers/Angular/NgModelHelpersFixture.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reload.Web.Helpers.Angular;
+
+namespace Reload.Web.Tests.Helpers.Angular
+{
+	[TestClass]
+	public class NgModelHelpersFixture
+	{
+		private static HtmlHelper<NgTestModel> TestHtmlHelper;
+
+		[ClassInitialize]
+		public static void Initialize(TestContext testContext)
+		{
+			TestHtmlHelper = HtmlHelperMockBetter.GetHtmlHelper<NgTestModel>(new NgTestModel());
+		}
+
+		[TestMethod]
+		public void MustReturnElementWithRequiredAttribute()
+		{
+			string element = TestHtmlHelper.NgModelFor(s => s.RequiredProperty).ToString();
+
+			Assert.IsTrue(element.StartsWith("<input"));
+
+			Assert.IsTrue(element.Contains("required"));
+		}
+	}
+}
---
+++ UnitTests/Reload.Web.Tests/Helpers/HtmlHelperMockBetter.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+
+namespace Reload.Web.Tests.Helpers
+{
+	internal static class HtmlHelperMockBetter
+	{
+		/// <summary>Gets the HTML helper.</summary>
+		/// <typeparam name="TModel">The type of the model.</typeparam>
+		/// <param name="inputDictionary">The input dictionary.</param>
+		/// <returns>A mocked HtmlHelper for unit testing.</returns>
+		public static HtmlHelper<TModel> GetHtmlHelper<TModel>(TModel inputDictionary)
+		{
+			ViewDataDictionary viewDataDictionary = new ViewDataDictionary(inputDictionary);
+
+			Mock<ViewContext> mockViewContext = new Mock<ViewContext>(
+				new Mock<ControllerContext>(
+					new Mock<HttpContextBase>().Object,
+					new RouteData(),
+					new Mock<ControllerBase>().Object
+				).Object,
+				new Mock<IView>().Object,
+				viewDataDictionary,
+				new TempDataDictionary(),
+				new Mock<TextWriter>().Object
+			);
+			
+			Mock<IViewDataContainer> mockDataContainer = new Mock<IViewDataContainer>();
+			mockDataContainer.Setup(c => c.ViewData).Returns(viewDataDictionary);
+
+			return new HtmlHelper<TModel>(mockViewContext.Object, mockDataContainer.Object);
+		}
+	}
+}
