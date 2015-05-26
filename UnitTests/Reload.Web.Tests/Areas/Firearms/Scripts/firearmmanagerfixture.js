a16e07f90bc0144788d115b26e37a8647e4af501
farleyrw <farleyrw@gmail.com>
2015-05-26 19:14:34 -0400

    finish firearm list unit tests

--- UnitTests/Reload.Web.Tests/Areas/Firearms/Scripts/firearmmanagerfixture.js
+++ UnitTests/Reload.Web.Tests/Areas/Firearms/Scripts/firearmmanagerfixture.js
@@ -20,16 +20,33 @@ describe('Firearm Manager tests', function () {
 	beforeEach(module('FirearmManager'));
 
 	describe('FirearmListController', function () {
+		var testEnum = { abc: '123' };
+		var testFirearm = { FirearmId: 123 };
+		var ConfirmDialog = {
+			Show: function () {
+				return {
+					then: function (callback) {
+						callback();
+					}
+				};
+			}
+		};
+
 		beforeEach(inject(function ($httpBackend, $rootScope, $controller, $location) {
 			this.httpBackend = $httpBackend;
-			this.httpBackend.expectGET('firearms/manage/list').respond([{ abc: '123' }]);
-			this.httpBackend.expectGET('/reload/firearms/enums/get').respond({ abc: '123' });
+			this.httpBackend.expectGET('firearms/manage/list').respond([testFirearm]);
+			this.httpBackend.expectGET('/reload/firearms/enums/get').respond(testEnum);
 
 			this.scope = $rootScope.$new();
-			this.controller = $controller('FirearmListController', { $scope: this.scope, $location: $location });
+			this.controller = $controller('FirearmListController', {
+				$scope: this.scope,
+				$location: $location,
+				ConfirmDialog: ConfirmDialog
+			});
 		}));
 
 		it('should create scope methods', function () {
+			expect(this.scope.Firearms).toBeDefined();
 			expect(this.scope.Add).toBeDefined();
 			expect(this.scope.Edit).toBeDefined();
 			expect(this.scope.Delete).toBeDefined();
@@ -38,13 +55,23 @@ describe('Firearm Manager tests', function () {
 		it('should create "Enums" model with data fetched from xhr', function () {
 			this.httpBackend.flush();
 
-			expect(this.scope.Enums.abc).toBe('123');
+			expect(this.scope.Enums.abc).toBe(testEnum.abc);
 		});
 
 		it('should create "Firearms" model with data fetched from xhr', function () {
 			this.httpBackend.flush();
 
-			expect(this.scope.Firearms[0].abc).toBe('123');
+			expect(this.scope.Firearms[0].FirearmId).toBe(testFirearm.FirearmId);
+		});
+
+		it('should delete firearm', function () {
+			this.httpBackend.expectPOST('firearms/manage/delete').respond(testFirearm);
+			this.scope.Firearms.push(testFirearm);
+			
+			this.scope.Delete(testFirearm);
+			this.httpBackend.flush();
+
+			expect(this.scope.Firearms).toEqual([]);
 		});
 	});
 });
