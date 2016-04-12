92310851808a43290283091125a7bbe73e7ba6f7
farleyrw <farleyrw@gmail.com>
2016-04-11 21:14:56 -0400

    add scripts for work

---
+++ Web/Scripts/reload/basemodelhelper.js
@@ -0,0 +1,61 @@
+﻿'use strict';
+
+Informz.DefineNamespace('Informz.System.BaseModelHelper', function ($) {
+	/// The model extender service.
+	this.ModelExtender = function () {
+		/// Extends the model implementation to include base functionality.
+		this.Extend = function (model) {
+			if (!model.hasOwnProperty('ModelState')) { return; }
+
+			for (var property in model) {
+				if (Object.prototype.toString.call(model[property]) === '[object Array]') {
+					for (var arrayIndex = 0; arrayIndex < model[property].length; arrayIndex++) {
+						this.Extend(model[property][arrayIndex]);
+					}
+				}
+			}
+
+			return $.extend(true, model, ModelExtensions);
+		};
+
+		/// The model state types.
+		this.ModelStateTypes = {
+			Added: 0,
+			Deleted: 1,
+			Detached: 2,
+			Modified: 3,
+			Unchanged: 4
+		};
+
+		/// Returns a new base model.
+		this.GetBaseModel = function (model) {
+			model.ModelState = this.ModelStateTypes.Added;
+
+			return this.Extend(model);
+		};
+
+		// The base model extensions.
+		var ModelExtensions = {
+			ModelStateTypes: this.ModelStateTypes,
+			IsRemoved: function () {
+				return (this.ModelState == this.ModelStateTypes.Deleted || this.ModelState == this.ModelStateTypes.Detached);
+			},
+			Remove: function () {
+				if (this.ModelState == this.ModelStateTypes.Added) {
+					// If it was "Added" then change to detached.  We don't simply remove the object because that would
+					// be inconsistent with how a saved object is removed and could cause unexpected behavior.
+					this.ModelState = this.ModelStateTypes.Detached;
+				} else if (this.ModelState != this.ModelStateTypes.Detached) { // If it is "Detached" there is no need to remove.
+					this.ModelState = this.ModelStateTypes.Deleted;
+				}
+			},
+			Restore: function () {
+				if (this.ModelState == this.ModelStateTypes.Detached) {
+					this.ModelState = this.ModelStateTypes.Added;
+				} else {
+					this.ModelState = this.ModelStateTypes.Unchanged;
+				}
+			}
+		};
+	};
+}, [jQuery]);
---
+++ Web/Scripts/reload/basemodelhelperfixture.js
@@ -0,0 +1,91 @@
+﻿
+/// <reference path="../../../../../web/App/scripts/jquery/jquery-2.1.3.js" />
+/// <reference path="../../../../../web/App/scripts/informz/informz.js" />
+/// <reference path="../../../../../web/App/scripts/informz/system/basemodelhelper.js" />
+
+'use strict';
+
+describe("Base Model Helper module tests", function () {
+	var BaseModelHelper = Informz.System.BaseModelHelper;
+	var ModelExtender = new BaseModelHelper.ModelExtender();
+
+	it('should have BaseModelHelper defined', function () {
+		expect(BaseModelHelper).toBeDefined();
+		expect(ModelExtender).toBeDefined();
+	});
+
+	it('should extend model with base functionality', function () {
+		var model = ModelExtender.GetBaseModel({ prop: 'A' });
+		
+		expect(model.prop).toBe('A');
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Added);
+		expect(model.ModelStateTypes).toBeDefined();
+		expect(model.IsRemoved).toBeDefined();
+		expect(model.Remove).toBeDefined();
+		expect(model.Restore).toBeDefined();
+		expect(model.IsRemoved()).toBe(false);
+	});
+
+	it('should read removed state correctly', function () {
+		var model = ModelExtender.GetBaseModel({});
+
+		model.ModelState = ModelExtender.ModelStateTypes.Deleted;
+		expect(model.IsRemoved()).toBe(true);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Detached;
+		expect(model.IsRemoved()).toBe(true);
+	});
+
+	it('should set removed state correctly', function () {
+		var model = ModelExtender.GetBaseModel({});
+
+		model.Remove();
+		expect(model.IsRemoved()).toBe(true);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Added;
+		model.Remove();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Detached);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Detached;
+		model.Remove();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Detached);
+
+		// Calling remove on any other state becomes deleted.
+		model.ModelState = ModelExtender.ModelStateTypes.Deleted;
+		model.Remove();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Deleted);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Modified;
+		model.Remove();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Deleted);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Unchanged;
+		model.Remove();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Deleted);
+	});
+
+	it('should set restore state correctly', function () {
+		var model = ModelExtender.GetBaseModel({});
+
+		model.ModelState = ModelExtender.ModelStateTypes.Detached;
+		model.Restore();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Added);
+
+		// Calling restore on any other state becomes unchanged.
+		model.ModelState = ModelExtender.ModelStateTypes.Added;
+		model.Restore();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Unchanged);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Deleted;
+		model.Restore();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Unchanged);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Modified;
+		model.Restore();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Unchanged);
+
+		model.ModelState = ModelExtender.ModelStateTypes.Unchanged;
+		model.Restore();
+		expect(model.ModelState).toBe(ModelExtender.ModelStateTypes.Unchanged);
+	});
+});
